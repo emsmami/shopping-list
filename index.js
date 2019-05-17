bd36f544cf754d94fafc4c6689292a4bf56b898f
Jill Casey <jillcaseycodes@gmail.com>
2019-05-17 13:30:56 -0700

    check items off toggle

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 $('#js-shopping-list-form').on("submit", event => {
     event.preventDefault(); //prevents normal submit behavior.
 
-    const newItem = this.find(
-        'input[name="shopping-list-entry"]').val(); //get the value from the form
+    const newItemName = $('#shopping-list-entry').val(); //get the value from the form
+    
 
-    $('.shopping-list').append(
+    const newItem = '<li><span class="shopping-item">' 
+    + newItemName + 
+    '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button>' + '  ' + '<button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>';
 
-    <li>
-        <span class="shopping-item">$(newItem)</span>
-        <div class="shopping-item-controls">
-          <button class="shopping-item-toggle">
-            <span class="button-label">check</span>
-          </button>
-          <button class="shopping-item-delete">
-            <span class="button-label">delete</span>
-          </button>
-        </div>
-      </li>
-    )
+    $('.shopping-list').append(newItem);
 
-};
+
+});
+
+$('.shopping-item-toggle').on("click", event =>{
+    event.preventDefault();
+
+
+    $(event.target).closest('div').siblings('span').toggleClass('shopping-item__checked');
+    
+});
